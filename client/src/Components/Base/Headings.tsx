320025e7a8a4ca425bade5b5286f165801d00551
Michael.altinisik1 <Michael.altinisik@iths.se>
2023-05-22 12:29:53 +0200

    Add light and dark theme text color change for Headings and Paragrapgh

--- client/src/Components/Base/Headings.tsx
+++ client/src/Components/Base/Headings.tsx
@@ -3,21 +3,62 @@ import { H1, H2, H3, H4, H5 } from 'src/utils/typography';
 interface HeadingProps {
   type: 'H1' | 'H2' | 'H3' | 'H4' | 'H5';
   content: string | number;
+  isLightTheme?: boolean;
 }
 
-const Heading = ({ type, content }: HeadingProps) => {
+const Heading = ({ type, content, isLightTheme = true }: HeadingProps) => {
   const getheadingType = ({ type, content }: HeadingProps) => {
     switch (type) {
       case 'H1':
-        return <h1 className={H1}>{content}</h1>;
+        return (
+          <h1
+            className={`${H1} ${
+              isLightTheme ? 'text-gray-900' : 'text-gray-50'
+            }`}
+          >
+            {content}
+          </h1>
+        );
       case 'H2':
-        return <h2 className={H2}>{content}</h2>;
+        return (
+          <h2
+            className={`${H2} ${
+              isLightTheme ? 'text-gray-900' : 'text-gray-50'
+            }`}
+          >
+            {content}
+          </h2>
+        );
       case 'H3':
-        return <h3 className={H3}>{content}</h3>;
+        return (
+          <h3
+            className={`${H3} ${
+              isLightTheme ? 'text-gray-900' : 'text-gray-50'
+            }`}
+          >
+            {content}
+          </h3>
+        );
       case 'H4':
-        return <h4 className={H4}>{content}</h4>;
+        return (
+          <h4
+            className={`${H4} ${
+              isLightTheme ? 'text-gray-900' : 'text-gray-50'
+            }`}
+          >
+            {content}
+          </h4>
+        );
       case 'H5':
-        return <h5 className={H5}>{content}</h5>;
+        return (
+          <h5
+            className={`${H5} ${
+              isLightTheme ? 'text-gray-900' : 'text-gray-50'
+            }`}
+          >
+            {content}
+          </h5>
+        );
     }
   };
   return getheadingType({ type, content });
--- client/src/Components/Base/Paragrapgh.tsx
+++ client/src/Components/Base/Paragrapgh.tsx
@@ -3,15 +3,28 @@ import { body, body2 } from 'src/utils/typography';
 interface ParagraphProps {
   type: 'body' | 'bodySmall';
   content: string | number;
+  isLightTheme?: boolean;
 }
 
-const Paragraph = ({ type, content }: ParagraphProps) => {
+const Paragraph = ({ type, content, isLightTheme = true }: ParagraphProps) => {
   return (
     <>
       {type === 'body' ? (
-        <p className={body}>{content}</p>
+        <p
+          className={`${body} ${
+            isLightTheme ? 'text-gray-900' : 'text-gray-50'
+          }`}
+        >
+          {content}
+        </p>
       ) : (
-        <p className={body2}>{content}</p>
+        <p
+          className={`${body2} ${
+            isLightTheme ? 'text-gray-900' : 'text-gray-50'
+          }`}
+        >
+          {content}
+        </p>
       )}
     </>
   );
