9802e0244bdc45b63126f08f5c3b7b18778cb9c8
Michael.altinisik1 <Michael.altinisik@iths.se>
2023-09-29 19:32:43 +0200

    Remove import types from server

--- client/src/Types/index.ts
+++ client/src/Types/index.ts
@@ -1,18 +1,92 @@
 //Prisma generated types
-import {
-  User,
-  Notice,
-  Project,
-  Address,
-  Department,
-  Time_report,
-  Role,
-  Leave,
-  Permission,
-  Status,
-  Type_of_leave,
-} from 'src/../../server/node_modules/.prisma/client/index';
+// import {
+//   User,
+//   Notice,
+//   Project,
+//   Address,
+//   Department,
+//   Time_report,
+//   Role,
+//   Leave,
+//   Permission,
+//   Status,
+//   Type_of_leave,
+// } from 'src/../../server/node_modules/.prisma/client/index';
 
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  salary: number;
+  permission: Permission;
+  date_of_birth: string;
+  created_at: Date;
+  updated_at: Date;
+  roleId: string | null;
+  departmentId: string | null;
+};
+type Notice = {
+  id: string;
+  created_at: Date;
+  description: string | null;
+  status: Status;
+  userId: string;
+};
+type Project = {
+  id: string;
+  name: string;
+  created_at: Date;
+  start_date: Date;
+  deadline: Date;
+  description: string | null;
+};
+type Address = {
+  id: string;
+  country: string;
+  city: string;
+  zip: string;
+  street: string | null;
+  departmentId: string | null;
+};
+type Department = {
+  id: string;
+  name: string;
+  budget: number;
+  created_at: Date;
+};
+type Time_report = {
+  id: string;
+  created_at: Date;
+  from: Date;
+  to: Date;
+  status: Status;
+  userId: string;
+};
+type Role = {
+  id: string;
+  name: string;
+  created_at: Date;
+};
+type Leave = {
+  id: string;
+  type_of_leave: Type_of_leave;
+  created_at: Date;
+  from: Date;
+  to: Date;
+  status: Status;
+  userId: string;
+};
+type Permission = 'ADMIN' | 'EMPLOYEE';
+type Status = 'ACCEPTED' | 'PENDING' | 'REJECTED';
+type Type_of_leave =
+  | 'SICK_LEAVE'
+  | 'ANNUAL_LEAVE'
+  | 'MATERNITY_LEAVE'
+  | 'PATERNITY_LEAVE'
+  | 'COMPENSATORY_LEAVE'
+  | 'UNPAID_LEAVE';
 type UserIncludeTypes = {
   role?: {
     name?: string;
